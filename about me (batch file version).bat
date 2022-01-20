9091189b902aa1c326180922ef082aa0c43984fc
justlaughs <98098641+justlaughs@users.noreply.github.com>
2022-01-20 22:13:49 +0530

    Add files via upload

---
+++ about me (batch file version).bat
@@ -0,0 +1,4 @@
+prompt Hi, I’m @justlaughs   I’m interested in Java,JS  I’m currently learning nultiple programming languages but i would focus on java  I’m looking to collaborate on game projects I will make  Reach me on the mail sinhaaatiksh@outlook.com  I use the OS Windows 11
+
+prompt this might have unicode glitches
+pause
